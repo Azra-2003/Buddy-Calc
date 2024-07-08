0261da70cb6ce6f906fdd5a19a6dee7d40ba298e
Azra Ally <allyazra2003@gmail.com>
2024-07-08 20:38:02 +0200

    done

---
+++ src/components/calculator.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import './calculator.css'
+
+const Calculator = () => {
+    const [display, setDisplay] = useState("");
+
+    // function handle click
+    const handleButtonClick = (value) => {
+        setDisplay(display + value);
+    };
+
+    // function calculate results
+    const calculateResults = () => {
+        try {
+            setDisplay(eval(display).toString());
+        } catch (error) {
+            setDisplay("Error • ᴖ •");
+        }
+    };
+
+    // clear display
+    const clearDisplay = () => {
+        setDisplay("");
+    };
+
+    return (
+        <div className="calculator">
+            <div className="display"> {display} </div>
+            <div>
+                {[
+                    "7",
+                    "8",
+                    "9",
+                    "+",
+                    "4",
+                    "5",
+                    "6",
+                    "-",
+                    "1",
+                    "2",
+                    "3",
+                    "*",
+                    "0",
+                    ".",
+                    "=",
+                    "/",
+                ].map((value) => (
+                    <button 
+                        key={value} 
+                        onClick={() => 
+                            value === '=' ? calculateResults() : handleButtonClick(value) 
+                        }
+                    >
+                        {value}
+                    </button>
+                ))}
+                <button onClick={clearDisplay}>C</button>
+            </div>
+        </div>
+    );
+};
+
+export default Calculator;
